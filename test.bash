d467251b9e57c52d3586f9414983f8bfc2c35493
Takuto Kanno <s21C1038JU@s.chibakoudai.jp>
2022-10-27 14:42:56 +0900

    judge a code

--- test.bash
+++ test.bash
@@ -1,3 +1,22 @@
 #!/bin/bash
+# SPDX-FileCopyrightText : 2022 Takuto Kanno
+# SPDX-License-Identifirt: BSD-3-Clause
 
-seq 5 | ./plus
+###下がエラー文###
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+###I/0 TEST###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+# &&は左が成功すると右を実行つまり、この場合返されてたresが0ならechoにOKが格納される
+
+[ "$res" = 0 ]  && echo OK
+exit $res
+
